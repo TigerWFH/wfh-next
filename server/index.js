218dd33ad7e8ac19ebc1bffa07ebf81c7b7a9cd1
monkey <fanghuawfh@gmail.com>
2024-07-14 19:23:19 +0800

    feat: 接socket.io

--- server/index.js
+++ server/index.js
@@ -12,21 +12,36 @@ const io = new Server(app, {
   }
 });
 
-const userId = null;
+let userId = null;
+let helperId = null;
+const userIdToSocket = {};
+const helperIdSocket = {};
 
 io.on('connect', (socket) => {
-  console.log('wfh----server---connect');
-  socket.on('register_user', (userId, ackFn) => {
-    console.log('wfh--server---register_user');
-    userId = userId;
-    setTimeout(() => {
-      socket.emit('record');
-    }, 1000);
+  socket.on('register_user', (usrId, ackFn) => {
+    console.log('wfh---server---regist-->', usrId);
+    userIdToSocket[userId] = socket;
+    userId = usrId;
+    // setTimeout(() => {
+    //   socket.emit('record');
+    // }, 1000);
     ackFn?.();
   });
+  socket.on('register_helper', (helperId, ackFn) => {
+    helperIdSocket[helperId] = socket;
+    helperId = helperId;
+  });
+  socket.on('get_user', (ackFn) => {
+    ackFn?.(userId);
+  });
   socket.on('record', (event, ackFn) => {
-    console.log('wfh----server---record', event);
-    ackFn?.(ackFn);
+    const playerSocket = userIdToSocket[helperId];
+    playerSocket.emit('record', event);
+    ackFn?.(event);
+  });
+  socket.on('start_record', () => {
+    const recordSocket = userIdToSocket[userId];
+    recordSocket.emit('record');
   });
 });
 
