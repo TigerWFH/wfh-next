8f31937779b9c15993e04fee0107ff5d840a8989
monkey <fanghuawfh@gmail.com>
2024-07-20 16:32:07 +0800

    feat: 更新实时直播代码

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,42 @@ import { record } from 'rrweb';
 const userId = 'wfh';
 let stopRecordFn = null;
 let cacheEvents: any[] = [];
-const window = {};
+const window: any = {};
 
-const socket = io('http://localhost:6789', {
+export function getWindowHeight(): number {
+  return (
+    window.innerHeight ||
+    (document.documentElement && document.documentElement.clientHeight) ||
+    (document.body && document.body.clientHeight)
+  );
+}
+
+export function getWindowWidth(): number {
+  return (
+    window.innerWidth ||
+    (document.documentElement && document.documentElement.clientWidth) ||
+    (document.body && document.body.clientWidth)
+  );
+}
+
+const socket = io('http://localhost:6789/clients', {
   query: {
-    name: 'monkey'
+    userId
   }
 });
 
 socket.on('connect', () => {
   console.log('wfh---client-connect');
-  socket.emit('register_user', userId, () => {
-    console.log('wfh----register_user');
-  });
 });
 
-socket.on('record', () => {
+socket.on('start_record', () => {
+  console.log('start_record');
   stopRecordFn = record({
     emit(event) {
       cacheEvents.push(event);
-      socket?.emit('record', event, (evt) => {
+      console.log('user_interact', event);
+      socket?.emit('user_interact', { userId, event }, (params) => {
         console.log('wfh---->record---ack');
-        cacheEvents = cacheEvents.filter((e) => e.timestamp !== evt.timestamp);
       });
     }
   });
@@ -72,8 +86,23 @@ export default function Home() {
     setCat('newCat');
   }, []);
 
+  const getSession = () => {
+    socket.emit('request_session', { userId }, (params) => {
+      const { code, data } = params;
+      if (code === 0) {
+        console.log('active_page', userId);
+        socket.emit('active_page', {
+          userId,
+          width: getWindowWidth(),
+          height: getWindowHeight()
+        });
+      }
+    });
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={getSession}>获取session</button>
       <div onClick={onFish}>change fish</div>
       <div onClick={onMonkey}>change monkey</div>
       <div onClick={onCat}>change cat</div>
